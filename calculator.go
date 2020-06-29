04c34121c049babacf97004f8f50b7c6ae3104ac
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-29 13:57:26 +0100

    Logic for simple arithmetic operations

--- calculator.go
+++ calculator.go
@@ -1,6 +1,26 @@
 package calculator
 
-// Sum adds two given numbers and returns their sum.
+import "fmt"
+
+// Sum returns the sum of two numbers.
 func Sum(firstNumber, secondNumber int) int {
 	return firstNumber + secondNumber
+}
+
+// Subtract  returns the difference between two numbers.
+func Subtract(firstNumber, secondNumber int) int {
+	return firstNumber - secondNumber
+}
+
+// Multiply  returns the product of two numbers.
+func Multiply(firstNumber, secondNumber int) int {
+	return firstNumber * secondNumber
+}
+
+// Divide  returns the quotient of two numbers.
+func Divide(firstNumber, secondNumber int) int {
+	if secondNumber == 0 {
+		fmt.Println("Number cannot be divided by zero.")
+	}
+	return firstNumber / secondNumber
 }
--- calculator_test.go
+++ calculator_test.go
@@ -8,9 +8,18 @@ import (
 )
 
 var _ = Describe("Calculator", func() {
-	Context("Know when two numbers", func() {
-		It("should be added", func() {
+	Context("Can do basic arithmetic operations and can", func() {
+		It("add two numbers", func() {
 			Expect(calculator.Sum(1, 1)).To(Equal(2))
+		})
+		It("substract a number from the first one", func() {
+			Expect(calculator.Subtract(2, 1)).To(Equal(1))
+		})
+		It("multiply two numbers", func() {
+			Expect(calculator.Multiply(2, 2)).To(Equal(4))
+		})
+		It("divide a number by another number", func() {
+			Expect(calculator.Divide(2, 2)).To(Equal(1))
 		})
 	})
 })
