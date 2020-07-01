a894a894f518d7c4473c64356001e1a93e8904fd
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-07-01 08:42:52 +0100

    Refactor client tests

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -13,39 +13,42 @@ var _ = Describe("Client", func() {
 		operation string
 	)
 
-	Context("Function ParseArguments", func() {
-		BeforeEach(func() {
-			args := []string{"./client", "multiply", "1", "2"}
+	var _ = Describe("Function ParseArguments", func() {
+		JustBeforeEach(func() {
 			operation, err = client.ParseArguments(args)
 		})
 
-		It("returns an error if the operator is not `sum` or `average`", func() {
-			Expect(err).Should(MatchError("operation not recognized"))
-			Expect(operation).To(Equal(""))
-		})
-	})
+		Context("If operator is not `sum` or `average`", func() {
+			BeforeEach(func() {
+				args = []string{"./client", "multiply", "1", "2"}
+			})
 
-	Context("Function ParseArguments", func() {
-		BeforeEach(func() {
-			args = []string{"./client", "sum", "1", "2", "3"}
-			operation, err = client.ParseArguments(args)
-		})
-
-		It("returns an error if the number of arguments is 0", func() {
-			Expect(err).Should(MatchError("expected 'sum' or 'average' with 2 numeric values"))
-			Expect(operation).To(Equal(""))
-		})
-	})
-
-	Context("Function ParseArguments", func() {
-		BeforeEach(func() {
-			args = []string{"./client", "sum", "1", "2"}
-			operation, err = client.ParseArguments(args)
+			It("returns an error saying the operation is not recognized", func() {
+				Expect(err).Should(MatchError("operation not recognized"))
+				Expect(operation).To(Equal(""))
+			})
 		})
 
-		It("returns no error", func() {
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(operation).To(Equal("sum"))
+		Context("Function ParseArguments", func() {
+			BeforeEach(func() {
+				args = []string{"./client", "sum", "1", "2", "3"}
+			})
+
+			It("returns an error if the number of arguments is 0", func() {
+				Expect(err).Should(MatchError("expected 'sum' or 'average' with 2 numeric values"))
+				Expect(operation).To(Equal(""))
+			})
+		})
+
+		Context("Function ParseArguments", func() {
+			BeforeEach(func() {
+				args = []string{"./client", "sum", "1", "2"}
+			})
+
+			It("returns no error", func() {
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(operation).To(Equal("sum"))
+			})
 		})
 	})
 })
